19ecb0042a13190bf04fc8fdeef5efed091f9647
Deepali Singh <deepalisingh2k@gmail.com>
2021-11-09 14:24:56 +0100

    set style of e/i key press and UI fixes

--- game.js
+++ game.js
@@ -37,6 +37,53 @@ console.log("hello world");
 
 */
 
+const setStyle = (hasWon, keyCode) =>{
+    const face = document.getElementById("face");
+    const arrow = keyCode === 69 ? document.getElementById("arrow1") : keyCode === 73 ? document.getElementById("arrow2") : null
+
+    if(hasWon == true){
+        face.src = "./images/smiley.png"
+    }
+    else{
+        face.src = "./images/frowny.jpg"
+    }
+
+    if (arrow.style.visibility == "hidden") {
+        arrow.style.visibility = "visible";
+        setTimeout(() =>{
+            arrow.style.visibility = "hidden";
+        }, 3000)
+    }
+
+
+    if (face.style.visibility == "hidden") {
+        face.style.visibility = "visible";
+        setTimeout(() =>{
+            face.style.visibility = "hidden";
+        }, 3000)
+    }
+}
+document.addEventListener('keydown', (e) => {
+    /*
+    1. catch event if e or i pressed
+    2. calculate winner
+    
+    */
+    if ((e.keyCode == 69) || (e.keyCode ==73)){
+        console.log("pressed e/i");
+         // probFunction to check if it has won or lost --> tells us if face is smiley or frowny 
+        // if smiley, then set face.src = ./images/smiley.png
+        // else set face.src = ./images/frowny.jpg
+        const hasWon = true;
+        const keyCode = e.keyCode;
+        setStyle(hasWon, keyCode)
+
+    }
+
+
+});
+
+
 raw_data = () => {
     return {
         build: "",
