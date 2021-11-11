9a0a3283291113de6a208eb8f247ef093c75df27
binti-p <padaliya.binti@gmail.com>
2021-11-12 02:55:20 +0530

    add data generation

--- tol/task.js
+++ tol/task.js
@@ -1,3 +1,17 @@
+var Experiment_time_start = 0;
+var Experiment_time_end = 0;
+
+var problem_time_start = 0;
+var problem_time_end = 0;
+
+var problem_times = [];     // time between attempt start and attempt end of successful solution
+var attempts_time = [];
+var excecution_time = [];  // time between first move and successful solution
+
+var first_move_times = [];
+var first_move = []
+var first_move_time = 0;
+
 var drawBoard = function(container, ball_placement, board_type) {
     var board = '<div class = tol_' + container + '><div class = tol_base></div>'
     if (container == 'your_board') {
@@ -16,7 +30,8 @@ var drawBoard = function(container, ball_placement, board_type) {
         } else {
           board += '<div class = special id = tol_peg_' + (p + 1) + ' onclick = "tapPeg(this.id)">'
         }
-      } else {
+      } 
+      else {
         board += '<div id = tol_peg_' + (p + 1) + ' >'
       }
       var peg = ball_placement[p]
@@ -48,8 +63,20 @@ var tapPeg = function(peg_id) {
     var open_spot = peg.indexOf(0)
     if (open_spot != -1) {
       peg[open_spot] = held_ball
+      curr_data[1] = colors[held_ball-1]
+      curr_data[2] = choice + 1
+      movestring = movestring + colors[held_ball-1].slice(0,1)+peg_id.slice(-1);
+      curr_data[5] = movestring;
       held_ball = 0
-      num_moves += 1
+      if(num_moves === 0){
+        first_move_time = new Date().getTime();
+        var firstmove = (first_move_time - problem_time_start) /1000 ;
+        first_move.push(firstmove);
+      }
+      num_moves += 1;
+      curr_data[7] = num_moves;
+      curr_data[6] = num_moves- moves[problem];
+      data.push(curr_data.slice())
     }
   }
   var b1 = getPractice();
@@ -74,12 +101,28 @@ var checkEquality = function(arr1, arr2) {
 }
 
 function onsuccess(){
+  problem_time_end = new Date().getTime();
+  var timeTaken = (problem_time_end - problem_time_start) / 1000;
+  attempts_time.push(timeTaken);
+
+   problem_times.push(attempts_time);
+   //excecution_time.push(timeTaken-first_move.slice(-1));
+   first_move_times.push(first_move);
+
+
+  
   original = [
     [1, 2, 0],
     [3, 0],
     [0]
   ];
   points = points + 3 - num_trials;
+  curr_data[14]=1
+  curr_data[15]=3-num_trials
+  curr_data[16]=points
+  curr_data[2] = 0;
+  curr_data[1] = "feedback";
+  data.push(curr_data.slice())
   held_ball=0;
   problem+=1;
   num_moves=0;
@@ -89,6 +132,13 @@ function onsuccess(){
 }
 
 function onfail(){
+  // problem_time_end = new Date().getTime();
+  // var timeTaken = (problem_time_end - problem_time_start) / 1000;
+  attempts_time.push(-1);
+  problem_time_start = new Date().getTime();
+  excecution_time.push(-1);
+  first_move.push(-1);
+
   original = [
     [1, 2, 0],
     [3, 0],
@@ -97,7 +147,15 @@ function onfail(){
   held_ball=0;
   num_moves=0;
   num_trials +=1;
+  movestring="";
+  curr_data[1] = "reset";
+  curr_data[11] = num_trials;
+  data.push(curr_data.slice())
   if(num_trials>2){
+    problem_times.push(attempts_time);
+    console.log("fail: ", first_move);
+    first_move_times.push(first_move);
+    first_move= [];
     problem+=1;
     num_trials=0;
   }
@@ -106,6 +164,10 @@ function onfail(){
 }
 
 function onfinish(){
+  Experiment_time_end = new Date().getTime();
+  var exp_time = (Experiment_time_end - Experiment_time_start) / 1000;
+  console.log("Total time taken is:", exp_time);
+  
   original = [
     [1, 2, 0],
     [3, 0],
@@ -115,16 +177,24 @@ function onfinish(){
   num_moves=0;
   num_trials=0;
   problem=0;
-  //download data
+
+  console.log("problem times: ", problem_times);
+  console.log("first move time: " , first_move_times);
+  download_csv_file();
 }
 
 function next(){
   prev_prob = problem;
+  problem_time_start = new Date().getTime();
+  attempts_time = [];
+  
+  movestring ="";
+  curr_data = [0,0,0,0,0,movestring,0,0,0,0,0,1,problem+1,moves[problem],0,0,points]
   var b1 = getPractice();
   document.getElementById('main').innerHTML = b1;
 }
 
-var currData= function(){
+var currInfo= function(){
   var problemno = problem+1
   var attempt = num_trials+1
   var out = '<div>allowed moves:' + moves[problem] + '</div><div>attempt:' + attempt+ '</div><div>problem number:' + problemno + '</div><div>your moves:' + num_moves+ '</div>'
@@ -132,24 +202,32 @@ var currData= function(){
 }
 
 var getPractice = function() {
+  if(problem === 0){
+    Experiment_time_start = new Date().getTime();
+  }
   if(problem>=configurations.length){
     var feedback = '<div><h1>To download data press the button below. Your points- '+ points +'</h1></div>'
     var button = '<div><input type= "button" onclick = "onfinish()">download</input></div>'
     return feedback + button
   }
   else if(prev_prob!== problem){
-    var probno = problem+1;
+    var probno = problem+1;    
     var text = '<div><h1>the next problem is problem number'+ probno+'. Press next to begin</h1></div>'
     var button = '<div><input type= "button" onclick = "next()">next</input></div>'
     return button +text
   }
   else if(checkEquality(original,configurations[problem])){
+    problem_time_end = new Date().getTime();
     var referenceBoard = drawBoard('your_board', original, 'ref')
     var goal = drawBoard('goal_board', configurations[problem],'target')
+
+    var time1 = problem_time_end - problem_time_start;
+    time1 = (time1/1000) % 1000;
+    var TimeTaken = '<p>You took ' + time1 + '<p> s for problem number: '+ problem + '</p> <br>';
     var canvas = '<div class = tol_canvas><div class="tol_vertical_line"></div></div>'
     var feedback = '<div><h1>You got it!!</h1></div>'
     var button = '<div><input type= "button" onclick = "onsuccess()">next</input></div>'
-    return canvas + referenceBoard + goal + feedback + button
+    return canvas + referenceBoard + goal + TimeTaken + feedback + button
   }
   // else if(num_moves>=moves[problem]){
   //   var referenceBoard = drawBoard('your_board', original, 'ref')
@@ -165,7 +243,7 @@ var getPractice = function() {
   //   return canvas + referenceBoard + goal + feedback + button
   // }
   else{
-    var currdata = currData()
+    var currinfo = currInfo()
     var referenceBoard = drawBoard('your_board', original, 'ref')
     var goal = drawBoard('goal_board', configurations[problem],'target')
     var canvas = '<div class = tol_canvas><div class="tol_vertical_line"></div></div>'
@@ -180,7 +258,7 @@ var getPractice = function() {
         '<div class = tol_hand_box></div><div class = tol_hand_label><strong>Current ball</strong></div>'
     }
     var button = '<div><br><br><input type= "button" onclick = "onfail()">reset</input></div>'
-    return canvas + currdata + referenceBoard + goal + hold_box + button
+    return canvas + currinfo + referenceBoard + goal + hold_box + button
   }
 }
 var prev_prob =0;
@@ -196,57 +274,57 @@ var configurations = [
     [1, 0, 0],
     [2, 0],
     [3]
-  ],
-  [
-    [1, 3, 0],
-    [2, 0],
-    [0]
-  ],
-  [
-    [1, 0, 0],
-    [2, 3],
-    [0]
-  ],
-  [
-    [2, 1, 0],
-    [3, 0],
-    [0]
-  ],
-  [
-    [3, 0, 0],
-    [2, 1],
-    [0]
-  ],
-  [
-    [2, 3, 0],
-    [0, 0],
-    [1]
-  ],
-  [
-    [0, 0, 0],
-    [2, 3],
-    [1]
-  ],
-  [
-    [2, 1, 3],
-    [0, 0],
-    [0]
-  ],
-  [
-    [2, 3, 1],
-    [0, 0],
-    [0]
-  ],
-  [
-    [3, 1, 0],
-    [2, 0],
-    [0]
-  ],
-  [
-    [3, 0, 0],
-    [2, 0],
-    [1]
-  ]
+  ]//,
+  // [
+  //   [1, 3, 0],
+  //   [2, 0],
+  //   [0]
+  // ],
+  // [
+  //   [1, 0, 0],
+  //   [2, 3],
+  //   [0]
+  // ],
+  // [
+  //   [2, 1, 0],
+  //   [3, 0],
+  //   [0]
+  // ],
+  // [
+  //   [3, 0, 0],
+  //   [2, 1],
+  //   [0]
+  // ],
+  // [
+  //   [2, 3, 0],
+  //   [0, 0],
+  //   [1]
+  // ],
+  // [
+  //   [0, 0, 0],
+  //   [2, 3],
+  //   [1]
+  // ],
+  // [
+  //   [2, 1, 3],
+  //   [0, 0],
+  //   [0]
+  // ],
+  // [
+  //   [2, 3, 1],
+  //   [0, 0],
+  //   [0]
+  // ],
+  // [
+  //   [3, 1, 0],
+  //   [2, 0],
+  //   [0]
+  // ],
+  // [
+  //   [3, 0, 0],
+  //   [2, 0],
+  //   [1]
+  // ]
 ];
 
 var moves = [2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
@@ -264,6 +342,50 @@ var held_ball = 0;
 
 
 
-var b1 = getPractice();
-document.getElementById('main').innerHTML = b1;
+var movestring = "";
 
+
+// ----each move
+// 0 - move execution time
+// 1 - last moved disc //
+// 2 - peg it was moved to //
+// 3 - choice start (abs)
+// 4 - choice end (abs)
+// 5 - move string //
+// 6 - excess moves//
+// 7 - your moves//
+// ----each attempt
+// 8 - first move time
+// 9 - execution time
+// 10 - solution time
+// 11 - number of attempts //
+// ----each prob
+// 12 - problem number //
+// 13 - min moves //
+// 14 - true/false //
+// 15 - problem score //
+// 16 - total score //
+
+
+
+
+
+
+var data = [];  
+var curr_data = [];
+
+function download_csv_file() {  
+
+   var csv = 'move_exec_time,last_disc,peg_to,choice_start,choice_end,moves,excess_moves,your_moves,first_move_time,exec_time,sol_time,attempt_no,prob_no,min_moves,success,prob_score,total_score\n';  
+
+   data.forEach(function(row) {  
+           csv += row.join(',');  
+           csv += "\n";  
+   });
+
+   var hiddenElement = document.createElement('a');  
+   hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);  
+   hiddenElement.target = '_blank';  
+   hiddenElement.download = 'TOL.csv';  
+   hiddenElement.click();  
+}  
