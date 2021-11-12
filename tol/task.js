6ea26554fc74b584c7f007941be252e88c9d2dae
binti-p <padaliya.binti@gmail.com>
2021-11-12 23:32:29 +0530

    add css

--- tol/task.js
+++ tol/task.js
@@ -1,24 +1,22 @@
-var Experiment_time_start = 0;
-var Experiment_time_end = 0;
-
+//global variable to store the time for start of each attempt
 var problem_time_start = 0;
+
+//global variable to store the time for the ending of each attempt
 var problem_time_end = 0;
 
-var problem_times = [];     // time between attempt start and attempt end of successful solution
-var attempts_time = [];
-var excecution_time = [];  // time between first move and successful solution
-
-var first_move_times = [];
-var first_move = []
+//global variables used for intermediate calculations
+var firstmove=0;
 var first_move_time = 0;
-
-var allmove = 0;
-var str_allmove_level = [];
-var str_allmove_attempt = [];
-var str_allmove_exp = [];
-
 var move_start_time = 0;
 
+//The array moves stores the minimum number of moves for all the 12 problems
+var moves = [2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
+
+/*
+Function which returns the HTML code which corresponds to drawing the board
+with the 3 pegs and the balls on them according to ball_placement
+ref indicates that the board has to be responsive(the board on which the user can move balls)
+*/
 var drawBoard = function(container, ball_placement, board_type) {
     var board = '<div class = tol_' + container + '><div class = tol_base></div>'
     if (container == 'your_board') {
@@ -62,7 +60,8 @@ var tapPeg = function(peg_id) {
     for (var i = peg.length - 1; i >= 0; i--) {
       if (peg[i] !== 0) {
         held_ball = peg[i]
-        peg[i] = 0
+        peg[i] = 0;
+        move_start_time = new Date().getTime();
         break;
       }
     }
@@ -76,22 +75,21 @@ var tapPeg = function(peg_id) {
       var t1 = new Date().getTime();
       var t2 = t1 - move_start_time;
       t2 /= 1000;
-      str_allmove_attempt.push(t2);
-      move_start_time = new Date().getTime();
 
 
       movestring = movestring + colors[held_ball-1].slice(0,1)+peg_id.slice(-1);
       curr_data[5] = movestring;
       held_ball = 0
-      if(num_moves === 0 && num_trials <= 2){
-        move_start_time = new Date().getTime();
+      if(num_moves === 0){
         first_move_time = new Date().getTime();
-        var firstmove = (first_move_time - problem_time_start) /1000 ;
-        first_move.push(firstmove);
+        firstmove = (move_start_time-problem_time_start) /1000 ;
       }
+      curr_data[4] = t2;
+      curr_data[3] = "move";
+      curr_data[8] = firstmove;
       num_moves += 1;
       curr_data[7] = num_moves;
-      curr_data[6] = num_moves- moves[problem];
+      curr_data[6] = num_moves - moves[problem];
       data.push(curr_data.slice())
     }
   }
@@ -99,6 +97,11 @@ var tapPeg = function(peg_id) {
   document.getElementById('main').innerHTML = b1;
 }
 
+/*
+Function which checks if two nested arrays are equal
+after every move, this function will be called to check if the configuration of user's board
+is equal to the target board
+*/
 var checkEquality = function(arr1, arr2) {
   if (arr2.length !== arr1.length){
     return false;
@@ -116,23 +119,21 @@ var checkEquality = function(arr1, arr2) {
   return true;
 }
 
+/*
+Function that will be executed when the user's attempt is successful 
+->calculates the time for this attempt
+->appends the time for this problem to problem_times
+->appends the data of the current problem to data
+->Resets the original board configuration for the next problem
+->Resets the global variables num_moves, held_ball and num_trials for the next problem
+->Increments the problem number
+->Adds the score of the current problem to the global variable points
+*/
 function onsuccess(){
   problem_time_end = new Date().getTime();
   var timeTaken = (problem_time_end - problem_time_start) / 1000;
-  attempts_time.push(timeTaken);
-
-   problem_times.push(attempts_time);
-   //excecution_time.push(timeTaken-first_move.slice(-1));
-   first_move_times.push(first_move);
-   str_allmove_level.push(str_allmove_attempt);
-   str_allmove_exp.push(str_allmove_level);
-  //  if(str_allmove_attempt.length > 0){
-  //   str_allmove_level.push(str_allmove_attempt);
-  //   str_allmove_attempt = [];
-  // }
-   str_allmove_attempt = [];
-   str_allmove_level = [];
-
+  curr_data[9] = timeTaken - curr_data[8];
+  curr_data[10] = timeTaken;
 
   original = [
     [1, 2, 0],
@@ -144,7 +145,10 @@ function onsuccess(){
   curr_data[15]=3-num_trials
   curr_data[16]=points
   curr_data[2] = 0;
-  curr_data[1] = "feedback";
+  curr_data[4] = 0;
+  curr_data[3] = "success";
+  curr_data[1] = "NaN";
+  curr_data[2] = 0;
   data.push(curr_data.slice())
   held_ball=0;
   problem+=1;
@@ -154,28 +158,16 @@ function onsuccess(){
   document.getElementById('main').innerHTML = b1;
 }
 
+/*
+Function that will be executed when the user's attempt is unsuccessful
+->Resets the variable problem_time_start for the next attempt if the previous attempt is not the last attempt
+->Also resets move_start_time
+->Resets the original board configuration for the next problem
+->Resets the global variables num_moves, held_ball for the next problem
+->Increments num_trials as there already has been one unsuccessful attempt
+->Increments problem variable for the next problem if the previous attempt was the last attempt.
+*/
 function onfail(){
-  // problem_time_end = new Date().getTime();
-  // var timeTaken = (problem_time_end - problem_time_start) / 1000;
-  if(num_trials <= 2){
-    attempts_time.push(-1);
-    problem_time_start = new Date().getTime();
-    move_start_time = new Date().getTime();
-    //excecution_time.push(-1);
-    if(first_move.length > num_trials) {
-      first_move.pop();
-    }
-    first_move.push(-1);
-  }if(num_trials > 2){
-    first_move = [-1,-1,-1];
-  }
-
-  if(str_allmove_attempt.length > 0){
-    str_allmove_level.push(str_allmove_attempt);
-    str_allmove_attempt = [];
-  }
-
-
   original = [
     [1, 2, 0],
     [3, 0],
@@ -185,14 +177,19 @@ function onfail(){
   num_moves=0;
   num_trials +=1;
   movestring="";
-  curr_data[1] = "reset";
-  curr_data[11] = num_trials;
+  curr_data[1] = "NaN";
+  curr_data[2] = 0;
+  curr_data[4] = 0;
+  curr_data[3] = "reset";
+  curr_data[9] = -1;
+  curr_data[10] = -1;
   data.push(curr_data.slice())
+  curr_data[9] = 0;
+  curr_data[10] = 0;
+  curr_data[11] = num_trials+1;
   if(num_trials>2){
-    problem_times.push(attempts_time);
-    console.log("fail: ", first_move);
-    first_move_times.push(first_move);
-    first_move= [];
+    problem_time_start = new Date().getTime();
+    move_start_time = new Date().getTime();
     problem+=1;
     num_trials=0;
   }
@@ -200,12 +197,15 @@ function onfail(){
   document.getElementById('main').innerHTML = b1;
 }
 
+/*
+This function will be called when the experiment is finished and the download button is pressed by the user.
+->Calculates the total time for the experiment
+->Resets all the global variables
+->Resets the original configuration
+->Calculates the execution time for all the problems
+->Invokes the function for downloading the .csv 
+*/
 function onfinish(){
-  Experiment_time_end = new Date().getTime();
-  var exp_time = (Experiment_time_end - Experiment_time_start) / 1000;
-  console.log("Total time taken is:", exp_time);
-
-
   original = [
     [1, 2, 0],
     [3, 0],
@@ -215,72 +215,48 @@ function onfinish(){
   num_moves=0;
   num_trials=0;
   problem=0;
-
-  // adding excecution time = time solution - first move time
-  for(var i = 0; i<first_move_times.length; i++){
-    var tmp = [];
-    for(var j=0; j<first_move_times[i].length; j++){
-      if(first_move_times[i][j] === -1){
-        tmp.push(-1);
-      }else{
-        tmp.push(problem_times[i][j] - first_move_times[i][j]);
-      }
-    }
-    excecution_time.push(tmp);
-  }
-
-
-
-  console.log("problem times: ", problem_times);
-  console.log("first move time: " , first_move_times);
-  console.log("all moves: ", str_allmove_exp);
-  console.log("exxcecution time: ", excecution_time);
   download_csv_file();
 }
 
+/*
+The next function will be called at the start of the next problem.
+->Resets movestring, problem_time_start and move_start_time for the next problem and move
+->Initializes the row to be appended in the .csv file
+->Calls getPractice for the next problem to appear.
+*/
 function next(){
   prev_prob = problem;
   problem_time_start = new Date().getTime();
   move_start_time = new Date().getTime();
-  attempts_time = [];
-
   movestring ="";
   curr_data = [0,0,0,0,0,movestring,0,0,0,0,0,1,problem+1,moves[problem],0,0,points]
   var b1 = getPractice();
   document.getElementById('main').innerHTML = b1;
 }
 
+/*
+Function which shows the information about the user's current attempt
+->Shows the problem number, the attempt number, the allowed number of moves and the user's current number of moves.
+*/
 var currInfo= function(){
   var problemno = problem+1
   var attempt = num_trials+1
-  var out = '<div>allowed moves:' + moves[problem] + '</div><div>attempt:' + attempt+ '</div><div>problem number:' + problemno + '</div><div>your moves:' + num_moves+ '</div>'
+  var out = '<div class = "score_canva"><div>allowed moves:' + moves[problem] + '</div><div>attempt:' + attempt+ '</div><div>problem number:' + problemno + '</div><div>your moves:' + num_moves+ '</div></div>'
   return out
 }
 
+
 var getPractice = function() {
-  if(problem === 0){
-    Experiment_time_start = new Date().getTime();
-    if(str_allmove_level.length > 0){
-      str_allmove_exp.push(str_allmove_level);
-      str_allmove_level = [];
-    }
-  }
   if(problem>=configurations.length){
-    var feedback = '<div><h1>To download data press the button below. Your points- '+ points +'</h1></div>'
-    var button = '<div><input type= "button" onclick = "onfinish()">download</input></div>'
+    var feedback = '<div class = "text2"><h1>To download data press the button below. Your points is: '+ points +'</h1></div>'
+    var button = '<div><input type= "button" class="button-two" onclick = "onfinish()" value="Download"></input></div>'
     return feedback + button
   }
   else if(prev_prob!== problem){
-    first_move = [];
-    if(str_allmove_level.length > 0){
-      str_allmove_exp.push(str_allmove_level);
-      str_allmove_level = [];
-    }
-
     var probno = problem+1;
-    var text = '<div><h1>the next problem is problem number'+ probno+'. Press next to begin</h1></div>'
-    var button = '<div><input type= "button" onclick = "next()">next</input></div>'
-    return button +text
+    var text = '<div  class = "text2"><h1>the next problem is problem number '+ probno+'. <br> Press next to begin</h1></div>'
+    var button = '<div><input type= "button" class="button-two" onclick = "next()" value="Next" ></input></div>'
+    return text + button
   }
   else if(checkEquality(original,configurations[problem])){
     problem_time_end = new Date().getTime();
@@ -289,25 +265,12 @@ var getPractice = function() {
 
     var time1 = problem_time_end - problem_time_start;
     time1 = (time1/1000) % 1000;
-    var TimeTaken = '<p>You took ' + time1 + '<p> s for problem number: '+ problem + '</p> <br>';
+    var TimeTaken = '<div  class = "text2"> <p>You took ' + time1 + 's for problem number: '+ (problem+1) + '</p></div> <br>';
     var canvas = '<div class = tol_canvas><div class="tol_vertical_line"></div></div>'
     var feedback = '<div><h1>You got it!!</h1></div>'
-    var button = '<div><input type= "button" onclick = "onsuccess()">next</input></div>'
+    var button = '<div><input type= "button" class="button-two" onclick = "onsuccess()" value="Next"></input></div>'
     return canvas + referenceBoard + goal + TimeTaken + feedback + button
   }
-  // else if(num_moves>=moves[problem]){
-  //   var referenceBoard = drawBoard('your_board', original, 'ref')
-  //   var goal = drawBoard('goal_board', configurations[problem],'target')
-  //   var canvas = '<div class = tol_canvas><div class="tol_vertical_line"></div></div>'
-  //   if(num_trials>=2){
-  //     var feedback = '<div><h1>You have exceeded number of trials</h1></div>'
-  //   }
-  //   else{
-  //     var feedback = '<div><h1>Try again :(</h1></div>'
-  //   }
-  //   var button = '<div><input type= "button" onclick = "onfail()">next</input></div>'
-  //   return canvas + referenceBoard + goal + feedback + button
-  // }
   else{
     var currinfo = currInfo()
     var referenceBoard = drawBoard('your_board', original, 'ref')
@@ -323,13 +286,21 @@ var getPractice = function() {
       hold_box =
         '<div class = tol_hand_box></div><div class = tol_hand_label><strong>Current ball</strong></div>'
     }
-    var button = '<div><br><br><input type= "button" onclick = "onfail()">reset</input></div>'
+    var button = '<div><br><br><input type= "button" class="button-two" onclick = "onfail()" value="Reset"></input></div>'
     return canvas + currinfo + referenceBoard + goal + hold_box + button
   }
 }
+
+//The variable prev_prob stores the number for the previous problem
 var prev_prob =0;
+
+// The variable points stores the total points for the experiment
 var points = 0;
+
+//The variable problem stores the current problem number(0 for the 1st problem, 1 for the 2nd problem and so on)
 var problem = 0;
+
+//The variable configurations stores the arrangements of the target board for all the 12 problems
 var configurations = [
   [
     [0, 0, 0],
@@ -393,56 +364,77 @@ var configurations = [
   // ]
 ];
 
+
+//The array moves stores the minimum number of moves for all the 12 problems
 var moves = [2, 2, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5]
 
+//The variable num_moves stores the number of moves for the current attempt
 var num_moves = 0;
+
+//The variable num_trials stores the number of attempts for the current problem
 var num_trials =0;
+
+//the colors array will have green at the 0th index, red at the 1st index and blue at the 2nd index
 var colors = ['Green', 'Red', 'Blue'];
+
+/*
+The variable original is an array which stores the original configuration for the board of the user
+The first list [1,2,0] stores the arrangement for the first peg(the peg with the maximum height)
+(green ball at the bottom, red ball above it and empty space at the top- 0 denotes empty space)
+Similarly, the other two component arrays store the arrangements for the other 2 pegs.
+*/
 var original = [
   [1, 2, 0],
   [3, 0],
   [0]
 ];
 
+//held_ball is an integer variable denotes the state if a ball is currently held by the user.
+// held_ball is 1 if the user currently holds a ball
+// else it is zero
 var held_ball = 0;
 
-
-
+// stores the moves executed by the user( Eg-"B2R3" indicates that the blue ball (B) was moved to peg 2 (center) and the red ball (R) was moved to peg 3 (right).)
 var movestring = "";
 
 
+// 0 - id 
 // ----each move
-// 0 - move execution time
-// 1 - last moved disc //
-// 2 - peg it was moved to //
-// 3 - choice start (abs)
-// 4 - choice end (abs)
-// 5 - move string //     1
-// 6 - excess moves//     1
-// 7 - your moves//       1
+// 1 - last moved disc 
+// 2 - peg it was moved to 
+// 3 - choice type
+// 4 - move execution time
+// 5 - move string 
+// 6 - excess moves
+// 7 - your moves
 // ----each attempt
-// 8 - first move time    1
-// 9 - execution time     0
-// 10 - solution time     1
-// 11 - no of attempts // 1
+// 8 - first move time 
+// 9 - execution time 
+// 10 - solution time
+// 11 - no of attempts
 // ----each prob
-// 12 - problem number // 1
-// 13 - min moves //      0
-// 14 - true/false //     1
-// 15 - problem score //  1
-// 16 - total score //    1
+// 12 - problem number
+// 13 - min moves
+// 14 - true/false
+// 15 - problem score
+// 16 - total score
 
-
-
-
-
-
+//data stores the content of the entire csv file
 var data = [];
+
+/*
+curr_data stores the current row of the csv file
+*/
 var curr_data = [];
 
+/*
+Function invoked by the call in on_finish function, downloads the csv file containing data of the experiment 
+->adds commas for the csv format in each row
+->names the columns
+->saves the file as TOL.csv
+*/
 function download_csv_file() {
-
-   var csv = 'move_exec_time,last_disc,peg_to,choice_start,choice_end,moves,excess_moves,your_moves,first_move_time,exec_time,sol_time,attempt_no,prob_no,min_moves,success,prob_score,total_score\n';
+   var csv = 'user_id,last_disc,peg_to,choice,move_exec_time,moves,excess_moves,your_moves,first_move_time,exec_time,sol_time,attempt_no,prob_no,min_moves,success,prob_score,total_score\n';
 
    data.forEach(function(row) {
            csv += row.join(',');
